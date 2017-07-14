1b8dc9d7908a7379b967abf3ab4256f88989512e
naxido96 <nacholopez645@gmail.com>
2017-07-14 15:30:08 -0500

    avance de la pagina prtafolio

---
+++ script.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function($){
+ $('img.imagewarp').imageWarp() //apply warp effect to images with CSS class "imagewarp"
+ warpfactor: 5.5, //default increase factor of enlarged image
+duration: 1000, //default duration of animation, in millisec
+
+ $( ".navlink" ).click();
+})
